1b5cfcbef54efca053142a8b12961e425d5d65d6
Daniel Succar <89500097+Staay1@users.noreply.github.com>
2021-11-04 02:48:01 -0300

    Add files via upload

---
+++ SECAO 8/exercicio fixacao/src/Entities/Calculator.java
@@ -0,0 +1,19 @@
+package Entities;
+
+public class Calculator {
+
+	public static final double IOF = 0.06;
+	
+	
+	public static double tax(double convert) {
+		return (convert * IOF) + convert;
+	}
+	
+		
+}
+		
+
+	
+
+	
+	
